bd291fbcff1386c0c9ee95bd4bc759ace9e05709
Milo Koskela <115208829+kosmilo@users.noreply.github.com>
2024-01-14 15:51:24 +0200

    Unity version change

--- UpsideDown/Assets/Scripts/PlayerMovement.cs
+++ UpsideDown/Assets/Scripts/PlayerMovement.cs
@@ -6,13 +6,13 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 0.5f;
     [SerializeField] float jumpForce = 15f;
-    public Rigidbody2D rb2d;
+    public Rigidbody2D rb;
     private BoxCollider2D boxCollider2d;
     [SerializeField] private LayerMask platformsLayerMask;
 
     void Awake()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
         boxCollider2d = GetComponent<BoxCollider2D>();
     }
 
@@ -29,7 +29,7 @@ public class PlayerMovement : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+                rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
                 Debug.Log("Jump");
             }
         }
@@ -37,14 +37,14 @@ public class PlayerMovement : MonoBehaviour
 
     private bool IsGrounded()
     {
-        RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, rb2d.gravityScale * .1f, platformsLayerMask);
+        RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxCollider2d.bounds.center, boxCollider2d.bounds.size, 0f, Vector2.down, rb.gravityScale * .1f, platformsLayerMask);
         return raycastHit2d.collider != null;
     }
 
     public void GravitySwitch()
     {
         Debug.Log("Gravity Switched");
-        rb2d.gravityScale *= -1;
+        rb.gravityScale *= -1;
         jumpForce *= -1;
     }
 }
