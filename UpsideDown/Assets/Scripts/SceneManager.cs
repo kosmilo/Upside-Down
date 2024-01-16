df5de8bc1d05dd41cb2236ffb83a4be256f201dd
Milo Koskela <115208829+kosmilo@users.noreply.github.com>
2024-01-16 16:15:09 +0200

    Level redesigns and sprites

--- UpsideDown/Assets/Scripts/SceneManager.cs
+++ UpsideDown/Assets/Scripts/SceneManager.cs
@@ -5,20 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class SceneManager : MonoBehaviour
 {
-    public void StartGame() {
+    public void StartGame()
+    {
         UnityEngine.SceneManagement.SceneManager.LoadScene(1);
     }
 
-    public void NextScene() {
+    public void NextScene()
+    {
         UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void PreviousScene() {
+    public void PreviousScene()
+    {
         UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex - 1);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         NextScene();
+    }
+
+    public void MainMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
     }
 }
